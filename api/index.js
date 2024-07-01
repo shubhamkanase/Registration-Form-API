7b893783b5e4b0ac1bd63341663552767cb8bb71
Shubham Kanase <shubhamkanase92@gmail,com>
2024-07-02 00:27:34 +0530

    initial commit

--- api/index.js
+++ api/index.js
@@ -15,7 +15,7 @@ if (!fs.existsSync(uploadsDir)) {
 const app = express();
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "https://6682fbd0a39c305b2b499298--bright-alpaca-946876.netlify.app/",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
